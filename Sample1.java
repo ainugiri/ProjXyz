3e5a77311b4ded108ac4071ed297eaeced3519a5
Giri Prasad P <giriprasa@gmail.com>
2024-05-09 11:46:16 +0530

    github_desktop_commit

--- Sample1.java
+++ Sample1.java
@@ -1,5 +1,11 @@
 public class Sample1 {
+    public void display() {
+        System.out.println("Hello, World!");
+    }   
+
     public static void main(String[] args) {
+        
+
         System.out.println("Hello, World!");
     }
 }
--- index.js
+++ index.js
@@ -2,3 +2,10 @@ console.log('Hello World!');
 let a = 10;
 let b = 20;
 console.log(a + b);
+
+function calculate_no_of_days() {
+    let start_date = new Date('2021-01-01');
+    let end_date = new Date('2021-01-31');
+    let days = (end_date - start_date) / (1000 * 60 * 60 * 24);
+    console.log(days);
+}   
